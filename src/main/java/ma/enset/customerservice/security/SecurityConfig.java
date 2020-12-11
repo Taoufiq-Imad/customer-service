0494f7205f8be544e498c0aae4196032b35a9cf4
Taoufiq-Imad <Taoufiq-Imad@users.noreply.github.com>
2020-12-11 11:31:52 +0100

    update security setting

--- src/main/java/ma/enset/customerservice/security/SecurityConfig.java
+++ src/main/java/ma/enset/customerservice/security/SecurityConfig.java
@@ -13,6 +13,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.headers().frameOptions().disable();
         http.authorizeRequests().anyRequest().permitAll();
 //        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 //        http.csrf().disable();
